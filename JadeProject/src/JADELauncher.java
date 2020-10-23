fa8cad2e1566c31ce2606244420690718e511664
Ricardo <Cardoso>
2020-10-23 14:59:07 +0100

    helloWorldAgent working

--- JadeProject/src/JADELauncher.java
+++ JadeProject/src/JADELauncher.java
@@ -8,41 +8,22 @@ import jade.wrapper.StaleProxyException;
 
 public class JADELauncher {
 
-	public static void main(String[] args) {
-		Runtime rt = Runtime.instance();
+    public static void main(String[] args) {
+        Runtime rt = Runtime.instance();
 
-		Profile p1 = new ProfileImpl();
-		//p1.setParameter(...);
-		ContainerController mainContainer = rt.createMainContainer(p1);
-		
-		Profile p2 = new ProfileImpl();
-		//p2.setParameter(...);
-		ContainerController container = rt.createAgentContainer(p2);
+        Profile p1 = new ProfileImpl();
+        p1.setParameter(Profile.GUI, "true");
+        ContainerController mainContainer = rt.createMainContainer(p1);
 
-		AgentController ac1;
-		try {
-			ac1 = mainContainer.acceptNewAgent("name1", new Agent());
-			ac1.start();
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-		}
+        AgentController ac1;
+        try {
+            ac1 = mainContainer.acceptNewAgent("helloWorldAgent", new HelloWorldAgent());
+            ac1.start();
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
+        }
 
-		Object[] agentArgs = new Object[0];
-		AgentController ac2;
-		try {
-			ac2 = container.createNewAgent("name2", "jade.core.Agent", agentArgs);
-			ac2.start();
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-		}
 
-		AgentController ac3;
-		try {
-			ac3 = mainContainer.acceptNewAgent("myRMA", new jade.tools.rma.rma());
-			ac3.start();
-		} catch (StaleProxyException e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
 }
